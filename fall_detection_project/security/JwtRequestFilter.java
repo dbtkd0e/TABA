7124b08799383dbe78139aa53463e697859b141e
Yusang Oh <dbtkd0e@gmail.com>
2025-04-28 18:41:42 +0900

    Update JwtRequestFilter.java
    주석 제거(remove comment)

--- fall_detection_project/security/JwtRequestFilter.java
+++ fall_detection_project/security/JwtRequestFilter.java
@@ -30,37 +30,37 @@ public class JwtRequestFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
             throws ServletException, IOException {
             chain.doFilter(request,response);
-//        final String requestTokenHeader = request.getHeader("Authorization");
-//
-//        String username = null;
-//        String jwtToken = null;
-//
-//        if (requestTokenHeader != null && requestTokenHeader.startsWith("Bearer ")) {
-//            jwtToken = requestTokenHeader.substring(7);
-//            try {
-//                username = jwtTokenUtil.extractUsername(jwtToken);
-//            } catch (IllegalArgumentException e) {
-//                System.out.println("Unable to get JWT Token");
-//            } catch (ExpiredJwtException e) {
-//                System.out.println("JWT Token has expired");
-//            }
-//        } else {
-//            logger.warn("JWT Token does not begin with Bearer String");
-//        }
-//
-//        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-//
-//            UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
-//
-//            if (jwtTokenUtil.validateToken(jwtToken, userDetails)) {
-//
-//                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
-//                        userDetails, null, userDetails.getAuthorities());
-//                usernamePasswordAuthenticationToken
-//                        .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-//                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
-//            }
-//        }
-//        chain.doFilter(request, response);
+       final String requestTokenHeader = request.getHeader("Authorization");
+
+       String username = null;
+       String jwtToken = null;
+
+       if (requestTokenHeader != null && requestTokenHeader.startsWith("Bearer ")) {
+           jwtToken = requestTokenHeader.substring(7);
+           try {
+               username = jwtTokenUtil.extractUsername(jwtToken);
+           } catch (IllegalArgumentException e) {
+               System.out.println("Unable to get JWT Token");
+           } catch (ExpiredJwtException e) {
+               System.out.println("JWT Token has expired");
+           }
+       } else {
+           logger.warn("JWT Token does not begin with Bearer String");
+       }
+
+       if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+           UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+
+           if (jwtTokenUtil.validateToken(jwtToken, userDetails)) {
+
+               UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+                       userDetails, null, userDetails.getAuthorities());
+               usernamePasswordAuthenticationToken
+                       .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+               SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+           }
+       }
+       chain.doFilter(request, response);
     }
 }
